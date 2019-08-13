ef79e6cd2c0aef874b71404c918dc39ec2d5cf19
Mohamed Elbana <melbana@panarab-medila.com>
2019-08-13 03:22:31 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# About The project
+
+Simple IOS UIPanGesture Example.
+
+A pan gesture occurs any time the user moves one or more fingers around the screen. A screen-edge pan gesture is a specialized pan gesture that originates from the edge of the screen. Use the UIPanGestureRecognizer class for pan gestures and the UIScreenEdgePanGestureRecognizer class for screen-edge pan gestures.
+
+# Content Of This project
+
+- One ViewController contains 2 UIImageView one is the folder image which is applied on it UIPanGestureRecognizer to move and the other UIImageView is the Trush image.
+
+# Author
+
+Mohamed Elbana [LinkedIn Profile](https://www.linkedin.com/in/mohamed-elbana-a5a214ab)
+
+Created by Mohamed Elbana on 2019.
+Copyright © 2019 Mohamed Elbana.
+
+## Further Help
+
+To get more help on the project, you can contact with me by email address `m7amdelbana@gmail.com`
+
+## Note
+
+This project created on 2019, some code might be depreciated or become need to improve.
--- UIPanGesture/ViewController.swift
+++ UIPanGesture/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgFolder: UIImageView!
+    @IBOutlet weak var imgTrush: UIImageView!
+    
+    var folderViewOrigin: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addPanGesture(view: imgFolder)
+        folderViewOrigin = imgFolder.frame.origin
+        
+        view.bringSubviewToFront(imgFolder)
     }
-
-
+    
+    func addPanGesture(view: UIView) {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(sender:)))
+        view.addGestureRecognizer(pan)
+    }
+    
+    @objc func handlePan(sender: UIPanGestureRecognizer) {
+        let folderView = sender.view!
+     
+        switch sender.state {
+        case .began, .changed:
+            moveViewWithPan(view: folderView, sender: sender)
+            return
+        case .ended:
+            if imgFolder.frame.intersects(imgTrush.frame) {
+                self.deleteView(view: imgFolder)
+            } else {
+               self.returnViewToOrigin(view: imgFolder, origin: folderViewOrigin)
+            }
+            return
+        default:
+            return
+        }
+    }
+    
+    func moveViewWithPan(view: UIView, sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
+    }
+    
+    func deleteView(view: UIView) {
+        UIView.animate(withDuration: 0.3) {
+            view.alpha = 0.0
+        }
+    }
+    
+    func returnViewToOrigin(view: UIView, origin: CGPoint) {
+        UIView.animate(withDuration: 0.3) {
+            view.frame.origin = origin
+        }
+    }
 }
 
